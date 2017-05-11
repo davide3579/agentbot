b8cac821e1169b21a47a2be54e00b100bb8d2f6d
davide3579 <davideratti97@gmail.com>
2017-05-11 21:52:19 +0200

    commit generic changes

--- web/index.php
+++ web/index.php
@@ -3,7 +3,6 @@
 require('../vendor/autoload.php');
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider;
 
 
 $app = new Silex\Application();
@@ -14,8 +13,53 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
     'monolog.logfile' => 'php://stderr',
 ));
 
+// Register view rendering
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
 $app->post('/', function() use($app) {
-return new response ("ciao");
+
+    // parameters
+    $hubVerifyToken = 'wineshop093748';
+    $accessToken =   "EAAS3BvhSDrkBACpFikfYZCZCrHMFVdpKZAXqZCmwUPF0MHAHoYE9PHrEYTEJfLGqqF74n8mWbwVLEVCCdkdOhs0CvAFX5z9LRuiMW0ZCpqWfrJqH11ZAouP23Vw9m7J1vt5WbEwFODfRZBZA3OWmX9sZAaK5tcXjoTqYmpeNHgI5JTERWLFU1k2Rv";
+// check token at setup
+    if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
+        echo $_REQUEST['hub_challenge!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'];
+        exit;
+    }
+// handle bot's anwser
+    $input = json_decode(file_get_contents('php://input'), true);
+    $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
+    $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
+
+    $response = null;
+//set Message
+    if($messageText == "hi") {
+        $answer = "Hello";
+    }
+//send message to facebook bot
+    $response = [
+        'recipient' => [ 'id' => $senderId ],
+        'message' => [ 'text' => $answer ]
+    ];
+    $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    if(!empty($input)){
+        $result = curl_exec($ch);
+    }
+    curl_close($ch);
+
+    $app['monolog']->addDebug('logging output.!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+//  return $app['twig']->render('index.twig');
+    return new Response('Thank you for your feedback!', 201);
+
+});
+
+$app->get('/bot', function() use($app) {
+    return $app->render('bot.php');
 });
 
 $app->run();
