0abadffd6398b9269fbc84a1cdacdb9fb5a98517
davide3579 <davideratti97@gmail.com>
2017-06-06 21:06:49 +0200

    testing new DB

--- web/index.php
+++ web/index.php
@@ -8,6 +8,20 @@ use Symfony\Component\HttpFoundation\Response;
 $app = new Silex\Application();
 $app['debug'] = true;
 
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+    array(
+        'pdo.server' => array(
+            'driver'   => 'pgsql',
+            'user' => $dbopts["user"],
+            'password' => $dbopts["pass"],
+            'host' => $dbopts["host"],
+            'port' => $dbopts["port"],
+            'dbname' => ltrim($dbopts["path"],'/')
+        )
+    )
+);
+
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
     'monolog.logfile' => 'php://stderr',
@@ -56,6 +70,21 @@ $app->match("/", function (Request $request) use ($app) {
                 ]
             ]
         ]];
+        $response = [
+            'recipient' => [ 'id' => $senderId ],
+            'message' => $answer
+        ];
+    }else if($messageText == "db"){
+        $st = $app['pdo']->prepare('SELECT name FROM products');
+        $st->execute();
+
+        $names = array();
+        while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+            $app['monolog']->addDebug('Row ' . $row['name']);
+            $names[] = $row;
+        }
+
+        $answer = json_encode($names);
         $response = [
             'recipient' => [ 'id' => $senderId ],
             'message' => $answer
