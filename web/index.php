314e34182e775de0479a6c8d9ef988090b8eec0d
davide3579 <davideratti97@gmail.com>
2017-06-17 14:38:49 +0200

    test

--- web/index.php
+++ web/index.php
@@ -10,6 +10,20 @@ use Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider;
 $app = new Silex\Application();
 $app['debug'] = true;
 
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+    array(
+        'pdo.server' => array(
+            'driver'   => 'pgsql',
+            'user' => $dbopts["user"],
+            'password' => $dbopts["pass"],
+            'host' => $dbopts["host"],
+            'port' => $dbopts["port"],
+            'dbname' => ltrim($dbopts["path"],'/')
+        )
+    )
+);
+
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
     'monolog.logfile' => 'php://stderr',
@@ -22,179 +36,156 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 $app->match("/", function (Request $request) use ($app) {
 
-// parameters
-$hubVerifyToken = 'wineshop093748';
-$accessToken =   "EAAbq0pvXHasBABud3spgZBYyyen4yJ4ujgivSekB1ZACCCyaclqyr96uWNhVe8gaqEy50H1I3SO6dedt1UzNIt2zYZAY1fMy0bDzLJ0sjjZAvZASBDVcZC8UF4SZCRmnt67kE7DyL4MUijxhvzcZA9s5pRcHFDqQ9gRj2LHBkFuJlFgDBJZBBKG6Y";
+    // parameters
+    $hubVerifyToken = 'wineshop093748';
+    $accessToken =   "EAAbq0pvXHasBABud3spgZBYyyen4yJ4ujgivSekB1ZACCCyaclqyr96uWNhVe8gaqEy50H1I3SO6dedt1UzNIt2zYZAY1fMy0bDzLJ0sjjZAvZASBDVcZC8UF4SZCRmnt67kE7DyL4MUijxhvzcZA9s5pRcHFDqQ9gRj2LHBkFuJlFgDBJZBBKG6Y";
 // check token at setup
-if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
-    echo $_REQUEST['hub_challenge'];
-    exit;
-}
-
+    if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
+        echo $_REQUEST['hub_challenge'];
+        exit;
+    }
 // handle bot's anwser
-$input = json_decode(file_get_contents('php://input'), true);
-$senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
-$messageText = $input['entry'][0]['messaging'][0]['message']['text'];
-$payload =  $input['entry'][0]['messaging'][0]['postback']['payload'];
+    $input = json_decode(file_get_contents('php://input'), true);
+    $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
+    $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
+    $payload =  $input['entry'][0]['messaging'][0]['postback']['payload'];
 
-$response = null;
+    $response = null;
 //set Message
-if($messageText == "ciao"){
-    $answer = ["attachment"=>[
-        "type"=>"template",
-        "payload"=>[
-            "template_type"=>"button",
-            "text"=>"What do you want to do next?",
-            "buttons"=>[
-                [
-                    "type"=>"web_url",
-                    "url"=>"https://petersapparel.parseapp.com",
-                    "title"=>"Show Website"
-                ],
-                [
-                    "type"=>"postback",
-                    "title"=>"categorie",
-                    "payload"=>"CATEGORIES"
-                ]
-            ]
-        ]
-    ]];
-    $response = [
-        'recipient' => [ 'id' => $senderId ],
-        'message' => $answer
-    ];
-}
-
-if($payload == "USER_DEFINED_PAYLOAD"){
-    $answer = ["attachment"=>[
-        "type"=>"template",
-        "payload"=>[
-            "template_type"=>"list",
-            "elements"=>[
-                [
-                    "title"=> "Vino bianco",
-                    "image_url"=> "https://www.vinook.it/vino-bianco/vini-bianchi/vino-bianco-fermo_O1.jpg",
-                    "subtitle"=> "leggero,ma ubriacante",
-                    "default_action"=> [
-                        "type"=> "web_url",
-                        "url"=> "https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/",
-                        "webview_height_ratio"=> "tall",
-                        // "messenger_extensions"=> true,
-                        // "fallback_url"=> "https://peterssendreceiveapp.ngrok.io/"
+    if($messageText == "ciao"){
+        $answer = ["attachment"=>[
+            "type"=>"template",
+            "payload"=>[
+                "template_type"=>"button",
+                "text"=>"What do you want to do next?",
+                "buttons"=>[
+                    [
+                        "type"=>"web_url",
+                        "url"=>"https://petersapparel.parseapp.com",
+                        "title"=>"Show Website"
                     ],
-                    "buttons"=>[
-                        [
-                            "type"=>"web_url",
-                            "url"=>"https://petersfancybrownhats.com",
-                            "title"=>"dettagli"
-                        ],
-                    ]
-                ],
-                [
-                    "title"=>"vino rosso",
-                    "item_url"=>"https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/",
-                    "image_url"=>"https://upload.wikimedia.org/wikipedia/commons/8/88/Glass_of_Red_Wine_with_a_bottle_of_Red_Wine_-_Evan_Swigart.jpg",
-                    "subtitle"=>"Potente,dalla botta sicura",
-                    "buttons"=>[
-                        [
-                            "type"=>"web_url",
-                            "url"=>"https://petersfancybrownhats.com",
-                            "title"=>"View Website"
-                        ],
-                    ]
-                ],
-                [
-                    "title"=>"vino rosè",
-                    "item_url"=>"https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/",
-                    "image_url"=>"http://media.bellevy.com/images/5000-10000/1375443549-14581819.jpg",
-                    "subtitle"=>"da fighetti,per ogni tipo di aperitivo.",
-                    "buttons"=>[
-                        [
-                            "type"=>"web_url",
-                            "url"=>"https://petersfancybrownhats.com",
-                            "title"=>"View Website"
-                        ],
+                    [
+                        "type"=>"postback",
+                        "title"=>"categorie",
+                        "payload"=>"CATEGORIES"
                     ]
                 ]
             ]
-        ]
-    ]];
-    $response = [
-        'recipient' => [ 'id' => $senderId ],
-        'message' => $answer
-    ];}
-else if($payload == "CATEGORIES"){
+        ]];
+        $response = [
+            'recipient' => [ 'id' => $senderId ],
+            'message' => $answer
+        ];
+    }else if($messageText == "db"){
+        $st = $app['pdo']->prepare('SELECT nome FROM prodotti');
+        $st->execute();
 
-    $answer = ["attachment"=>[
-        "type"=>"template",
-        "payload"=>[
-            "template_type"=>"list",
-            "elements"=>[
-                [
-                    "title"=> "Vini bianchi",
-                    "image_url"=> "https://www.vinook.it/vino-bianco/vini-bianchi/vino-bianco-fermo_O1.jpg",
-                    "subtitle"=> "leggero,ma ubriacante",
-                    "default_action"=> [
-                        "type"=> "web_url",
-                        "url"=> "https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/",
-                        "webview_height_ratio"=> "tall",
-                        // "messenger_extensions"=> true,
-                        // "fallback_url"=> "https://peterssendreceiveapp.ngrok.io/"
+        $names = array();
+        while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+            $app['monolog']->addDebug('Row ' . $row['nome']);
+            $names[] = $row;
+        }
+
+        $answer = json_encode($names);
+        $response = [
+            'recipient' => [ 'id' => $senderId ],
+            'message' => $names[0]['nome']
+        ];
+    }
+
+    if($payload == "USER_DEFINED_PAYLOAD"){
+        $answer = ["attachment"=>[
+            "type"=>"template",
+            "payload"=>[
+                "template_type"=>"list",
+                "elements"=>[
+                    [
+                        "title"=> "Vino bianco",
+                        "image_url"=> "https://www.vinook.it/vino-bianco/vini-bianchi/vino-bianco-fermo_O1.jpg",
+                        "subtitle"=> "leggero,ma ubriacante",
+                        "default_action"=> [
+                            "type"=> "web_url",
+                            "url"=> "https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/",
+                            "webview_height_ratio"=> "tall",
+                            // "messenger_extensions"=> true,
+                            // "fallback_url"=> "https://peterssendreceiveapp.ngrok.io/"
+                        ],
+                        "buttons"=>[
+                            [
+                                "type"=>"web_url",
+                                "url"=>"https://petersfancybrownhats.com",
+                                "title"=>"dettagli"
+                            ],
+                        ]
                     ],
-                    "buttons"=>[
-                        [
-                            "type"=>"web_url",
-                            "url"=>"https://petersfancybrownhats.com",
-                            "title"=>"dettagli"
-                        ],
-                    ]
-                ],
-                [
-                    "title"=>"vini rossi",
-                    "item_url"=>"https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/",
-                    "image_url"=>"https://upload.wikimedia.org/wikipedia/commons/8/88/Glass_of_Red_Wine_with_a_bottle_of_Red_Wine_-_Evan_Swigart.jpg",
-                    "subtitle"=>"Potente,dalla botta sicura",
-                    "buttons"=>[
-                        [
-                            "type"=>"web_url",
-                            "url"=>"https://petersfancybrownhats.com",
-                            "title"=>"View Website"
-                        ],
-                    ]
-                ],
-                [
-                    "title"=>"vini rosè",
-                    "item_url"=>"https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/",
-                    "image_url"=>"http://media.bellevy.com/images/5000-10000/1375443549-14581819.jpg",
-                    "subtitle"=>"da fighetti,per ogni tipo di aperitivo.",
-                    "buttons"=>[
-                        [
-                            "type"=>"web_url",
-                            "url"=>"https://petersfancybrownhats.com",
-                            "title"=>"View Website"
-                        ],
+                    [
+                        "title"=>"vino rosso",
+                        "item_url"=>"https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/",
+                        "image_url"=>"https://upload.wikimedia.org/wikipedia/commons/8/88/Glass_of_Red_Wine_with_a_bottle_of_Red_Wine_-_Evan_Swigart.jpg",
+                        "subtitle"=>"Potente,dalla botta sicura",
+                        "buttons"=>[
+                            [
+                                "type"=>"web_url",
+                                "url"=>"https://petersfancybrownhats.com",
+                                "title"=>"View Website"
+                            ],
+                        ]
+                    ],
+                    [
+                        "title"=>"vino rosè",
+                        "item_url"=>"https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/",
+                        "image_url"=>"http://media.bellevy.com/images/5000-10000/1375443549-14581819.jpg",
+                        "subtitle"=>"da fighetti,per ogni tipo di aperitivo.",
+                        "buttons"=>[
+                            [
+                                "type"=>"web_url",
+                                "url"=>"https://petersfancybrownhats.com",
+                                "title"=>"View Website"
+                            ],
+                        ]
                     ]
                 ]
             ]
-        ]
-    ]];
-    $response = [
-        'recipient' => [ 'id' => $senderId ],
-        'message' => $answer
-    ];
-}
-$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
-curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-if(!empty($input)){
-    $result = curl_exec($ch);
-}
-curl_close($ch);
+        ]];
+        $response = [
+            'recipient' => [ 'id' => $senderId ],
+            'message' => $answer
+        ];}else if($payload == "CATEGORIES"){
 
-$app['monolog']->addDebug('logging output.!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+        $st = $app['pdo']->prepare('SELECT nome_cat FROM categorie');
+        $st->execute();
+
+        $names = array();
+        $buttons = array();
+
+        while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+            $app['monolog']->addDebug('Row-----categorie ' . $row['nome_cat']);
+            $names[] = $row['nome_cat'];
+            $button = array("type"=>"postback", "title"=> $row['nome_cat'], "payload"=> $row['nome_cat']);
+            array_push($buttons,$button);
+        }
+
+        $app['monolog']->addDebug(json_encode($buttons));
+
+        $answer = array("attachment" => array("type"=>"template","payload"=>array("template_type"=>"button","text"=>"select category","buttons"=>$buttons)));
+
+        $response = [
+            'recipient' => [ 'id' => $senderId ],
+            'message' => $answer
+        ];
+    }
+    $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    if(!empty($input)){
+        $result = curl_exec($ch);
+    }
+    curl_close($ch);
+
+    $app['monolog']->addDebug('logging output.!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
 //  return $app['twig']->render('index.twig');
-return new Response('Thank you for your feedback!', 201);
+    return new Response('Thank you for your feedback!', 201);
 
 });
 
